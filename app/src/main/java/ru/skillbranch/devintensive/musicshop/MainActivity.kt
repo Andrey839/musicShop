ce1f2e21c6efe1aa55881bc833651405977023ef
Andrey839 <robin839168@gmail.com>
2020-01-26 19:46:15 +0200

    Spiner work!

--- app/src/main/java/ru/skillbranch/devintensive/musicshop/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/musicshop/MainActivity.kt
@@ -4,32 +4,62 @@ import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var one = 0
+    val goodName = mapOf("guitars" to 1500, "drum" to 1000, "keyboard" to 500, "violin" to 2000)
+    lateinit var position: Any
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var one = 0
-        val goodInstrument = listOf("guitars","drum","keyboard","violin")
-        val spinnerAdapter =ArrayAdapter(this,android.R.layout.simple_spinner_item,goodInstrument)
+        val goodInstrument = listOf("guitars", "drum", "keyboard", "violin")
+        val spinnerAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, goodInstrument)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner2.adapter = spinnerAdapter
+
+
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val price = textView3
+                var position = spinner2.selectedItem
+                price.text = ((one)* (goodName[position])!!.toInt()).toString()
+            }
+        }
+
     }
 
     fun increaseQuantity(view: View) {
         val pluss = textView5
-        pluss.text = (one++).toString()
+        one++
+        pluss.text = one.toString()
+        val price = textView3
+        var position = spinner2.selectedItem
+        price.text = ((one)* (goodName[position])!!.toInt()).toString()
     }
-
     fun decreaseQuantity(view: View) {
         val minus = textView5
-        if (one > 0) minus.text = (one--).toString()
-        else minus.text = "0"
-    }
+        if (one > 0) {
+            one--
+            minus.text = one.toString()
+        } else {minus.text = "0"}
+        val price = textView3
+        var position = spinner2.selectedItem
+        price.text = ((one)* (goodName[position])!!.toInt()).toString()
 
+    }
 }

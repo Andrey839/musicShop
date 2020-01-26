3858c6147a38f5868fbffa12a07864d5e91af15c
Andrey839 <robin839168@gmail.com>
2020-01-26 17:13:11 +0200

    Buton + and -

--- app/src/main/java/ru/skillbranch/devintensive/musicshop/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/musicshop/MainActivity.kt
@@ -4,19 +4,32 @@ import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var one = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var one = 0
+        val goodInstrument = listOf("guitars","drum","keyboard","violin")
+        val spinnerAdapter =ArrayAdapter(this,android.R.layout.simple_spinner_item,goodInstrument)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.adapter = spinnerAdapter
     }
 
     fun increaseQuantity(view: View) {
-        val plus = textView5
-        var one = 1
-        plus.text = (one+1).toString()
+        val pluss = textView5
+        pluss.text = (one++).toString()
+    }
+
+    fun decreaseQuantity(view: View) {
+        val minus = textView5
+        if (one > 0) minus.text = (one--).toString()
+        else minus.text = "0"
     }
 
 }
